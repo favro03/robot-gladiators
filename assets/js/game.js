83b56bd9e5b06ddfdf35006dc758877f340fc3fd
tfavro <wetr9902@gmail.com>
2021-12-03 11:34:11 -0600

    window and console adds

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!")
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+var playerName = window.prompt("what is your robot's name?");
+console.log(playerName);
+console.log("This logs a stirng, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10+10);
+console.log("OUR ROBOT'S NAME IS " +playerName);
+
+//fight();
